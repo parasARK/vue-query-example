2e6f1c47f10c852ce9465601d947a64e3da1adde
parasARK <parass@arkinfosoft.in>
2022-07-30 20:36:59 +0530

    show new loader in list page

---
+++ src/components/Loader.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  size?: string
+  background?: string
+  duration?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: '40px',
+  background: '#41b883',
+  duration: '1.2s',
+})
+
+const styles = computed(() => ({
+  width: props.size,
+  height: props.size,
+}))
+
+const rectStyles = computed(() => ({
+  backgroundColor: props.background,
+  animationDuration: props.duration,
+}))
+</script>
+
+<template>
+  <div :style="styles" class="spinner spinner--stretch">
+    <div :style="rectStyles" class="rect rect-1"></div>
+    <div :style="rectStyles" class="rect rect-2"></div>
+    <div :style="rectStyles" class="rect rect-3"></div>
+    <div :style="rectStyles" class="rect rect-4"></div>
+    <div :style="rectStyles" class="rect rect-5"></div>
+  </div>
+</template>
+
+<style scoped>
+.spinner {
+  display: inline-block;
+  text-align: center;
+  font-size: 10px;
+}
+
+.spinner * {
+  line-height: 0;
+  box-sizing: border-box;
+}
+
+.rect {
+  margin-left: 2px;
+  margin-right: 2px;
+  animation: sketch 1.2s ease-in-out infinite;
+}
+.spinner > div {
+  height: 100%;
+  width: 10%;
+  display: inline-block;
+}
+.spinner .rect-2 {
+  animation-delay: -1.1s;
+}
+.spinner .rect-3 {
+  animation-delay: -1s;
+}
+.spinner .rect-4 {
+  animation-delay: -0.9s;
+}
+.spinner .rect-5 {
+  animation-delay: -0.8s;
+}
+@keyframes sketch {
+  0%,
+  40%,
+  100% {
+    transform: scaleY(0.3);
+  }
+  20% {
+    transform: scaleY(1);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
 import { computed, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import usePokemonList from '@/composables/usePokemonList'
+import Loader from '@/components/Loader.vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -26,8 +27,6 @@ const filteredPokemon = computed(() => {
     <div class="container mx-auto px-4">
       <h1 class="text-3xl font-bold mb-4">Pokemons List</h1>
 
-      <p v-if="isLoading" class="text-2xl text-slate-600 mb-3">Loading...</p>
-
       <form class="mb-4">
         <input
           type="search"
@@ -37,30 +36,35 @@ const filteredPokemon = computed(() => {
         />
       </form>
 
-      <section v-if="filteredPokemon">
-        <div
-          v-if="filteredPokemon?.length > 0"
-          class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4"
-        >
-          <router-link
-            v-for="(pokemon, index) in filteredPokemon"
-            :key="pokemon.name"
-            :to="{ name: 'pokemon-details', params: { name: pokemon.name } }"
-            class="p-3 block border-2 border-slate-100 rounded hover:shadow-md transition-all"
-          >
-            <div class="w-24 h-24 lg:w-40 lg:h-40 mx-auto">
-              <img
-                :src="`${BASE_IMAGE_URL}/${index + 1}.png`"
-                :alt="pokemon.name"
-                loading="lazy"
-                class="w-full h-full"
-              />
-            </div>
-            <p class="text-lg text-blue-600 text-center capitalize">{{ pokemon.name }}</p>
-          </router-link>
+      <section>
+        <div v-if="isLoading" class="grid place-items-center h-60">
+          <loader size="60px" />
         </div>
-        <div v-else>
-          <h4 class="text-3xl font-semibold">No Pokemon found</h4>
+        <div v-if="filteredPokemon">
+          <div
+            v-if="filteredPokemon?.length > 0"
+            class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4"
+          >
+            <router-link
+              v-for="(pokemon, index) in filteredPokemon"
+              :key="pokemon.name"
+              :to="{ name: 'pokemon-details', params: { name: pokemon.name } }"
+              class="p-3 block border-2 border-slate-100 rounded hover:shadow-md transition-all"
+            >
+              <div class="w-24 h-24 lg:w-40 lg:h-40 mx-auto">
+                <img
+                  :src="`${BASE_IMAGE_URL}/${index + 1}.png`"
+                  :alt="pokemon.name"
+                  loading="lazy"
+                  class="w-full h-full"
+                />
+              </div>
+              <p class="text-lg text-blue-600 text-center capitalize">{{ pokemon.name }}</p>
+            </router-link>
+          </div>
+          <div v-else>
+            <h4 class="text-3xl font-semibold">No Pokemon found</h4>
+          </div>
         </div>
       </section>
     </div>
